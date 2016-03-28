8a3161f7ea953934b29e60980fa75d3b439b7b4f
abeirm <abeir.moussa@gmail.com>
2016-03-28 12:19:05 -0500

    threading added to tesseract program

--- tesseract/tesseract_video.py
+++ tesseract/tesseract_video.py
@@ -1,32 +1,64 @@
 import numpy as np
 import cv2
 import subprocess
+import threading
+import os
 
-cap = cv2.VideoCapture(0)
+semaphore = threading.BoundedSemaphore();
 
-while(True):
-    # Capture frame-by-frame
-    station_name = "hello"
-    ret, frame = cap.read()
+def video():
+    global semaphore
+    cap = cv2.VideoCapture(0)
 
-    # Our operations on the frame come here
-    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        # Our operations on the frame come here
+        #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        saving_thread = threading.Thread(target=save_image,args=(frame,))
+        saving_thread.start()
+        
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+def save_image(frame):
+    print "saving image is started..."
+    semaphore.acquire()
     cv2.imwrite("output.png", frame)
-    subprocess.call("tesseract output.png out", shell=True)
+    semaphore.release()
+    print "saving image is done..."
+def tesseract():
 
-    tesseract_output = open("out.txt", "r")
+    while(True):
+        semaphore.acquire()
+        subprocess.call("tesseract output.png stdin", shell=True)
+        semaphore.release()
+
+
+def find_station(station_name):
+    global semaphore
+
+    #semaphore.acquire()
+    tesseract_output = input()
+    print tesseract_output
 
     for line in tesseract_output: 
-    	output_array = line.split();
-    	for word in output_array:
-    		if(word.lower() == "hello"):
-    			print "FOUND STATION!!!!!!!!!!!!"
+        output_array = line.split();
+        for word in output_array:
+            if(word.lower() == station_name):
+                print "FOUND STATION!!!!!!!!!!!!"
+    tesseract_output.close()
+    #semaphore.release()
 
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+video_thread = threading.Thread(target=video)
+tesseract_thread = threading.Thread(target=tesseract)
+station_finding_thread = threading.Thread(target=find_station, args="packet")
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+video_thread.start()
+tesseract_thread.start()
