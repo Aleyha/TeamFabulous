94f83ec2019e75c27712537de676c1454be68a48
kiranhamid <kiran.hamid@mavs.uta.edu>
2016-04-20 14:32:17 -0500

    added web stuff
    cgi stuff

---
+++ server/login.js
@@ -0,0 +1,16 @@
+function validateLogin(){
+
+	var user = document.forms["loginForm"]["uname"].value;
+	console.log(user);
+	var pass = document.forms["loginForm"]["password"].value;
+	console.log(pass);
+	if(user == "UTAfab" && pass == "password"){
+		alert("Access Granted");
+		return null;
+	}
+	else{
+		alert("Access Denied");
+		return null;
+	}
+
+}
