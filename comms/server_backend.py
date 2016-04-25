f093a1df7be71ccf3f7c50baf694ad44cfd1f345
christieg <christie.gutierrez@mavs.uta.edu>
2016-04-25 01:22:40 -0500

    messaging works on server now
    for now, anyways....

--- comms/server_backend.py
+++ comms/server_backend.py
@@ -7,31 +7,23 @@ import zmq
 context = zmq.Context()
 
 # setting up socket for communication with the main program
-socket = context.socket(zmq.ROUTER)
+socket = context.socket(zmq.DEALER)
 socket.setsockopt(zmq.IDENTITY, b'server')
 socket.connect('tcp://localhost:5550')
 
 poller = zmq.Poller()
 poller.register(socket, zmq.POLLIN)
-line_detection_in_use = False
-while True:
 
-    
-    station = raw_input("what station? ")
-    if line_detection_in_use:
-        print "line detection in use"
-        
-        print "polling for a message"
-        sockets = dict(poller.poll(1000))
-        if socket in sockets:
-            print socket.recv_multipart()
-            line_detection_in_use = False
-            
-    if not line_detection_in_use:
-        socket.send_multipart([b'main',b'server', station])
-        print socket.recv_multipart()
-        line_detection_in_use = True
+station = raw_input("what station? ")
 
-   
-
+socket.send_multipart([station])
+print "polling for a message"
+timeout = False
+sockets = dict(poller.poll(1000))
+if socket in sockets:
+    print socket.recv_multipart()
+else:
+    timeout = True
+    print "timeout"
+  
 socket.close()
