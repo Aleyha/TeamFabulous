af2c6850260e7b7c6f77ee5346ccd4f55dbcae72
abeirm <abeir.moussa@gmail.com>
2016-04-28 21:25:36 -0500

    fixed hi.sh

--- comms/hi.sh
+++ comms/hi.sh
@@ -1,5 +1,6 @@
 /etc/init.d/lighttpd restart
-python main_program.py & echo $! >> run.pid
-python line_detection.py & echo $! >> run.pid
-python motor.py & echo $! >> run.pid
-python server_backend.py & echo $! >> run.pid
+python /home/fart/TeamFabulous/comms/main_program.py & echo $! >> run.pid
+python /home/fart/TeamFabulous/lineDetection/line-detection.py & echo $! >> run.pid
+python /home/fart/TeamFabulous/comms/motor.py & echo $! >> run.pid
+#testing only
+#python server_backend.py & echo $! >> run.pid
