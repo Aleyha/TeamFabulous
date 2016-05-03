1d6c205f4f612bbe12c9836e01a8c289ce368986
christieg <christie.gutierrez@mavs.uta.edu>
2016-05-03 14:03:31 -0500

    arduino

--- motor/arduino/SerialTest/SerialTest.ino
+++ motor/arduino/SerialTest/SerialTest.ino
@@ -1,36 +1,22 @@
 
-//Arduino Mega and Leonardo chips only support some pins for receiving data back from the RoboClaw
-//This is because only some pins of these boards support PCINT interrupts or are UART receivers.
-//Mega: 0,10,11,12,13,14,15,17,19,50,51,52,53,A6,A7,A8,A9,A10,A11,A12,A13,A14,A15
-//Leonardo: 0,8,9,10,11
+#include <Sabertooth.h>
 
-//Arduino Due currently does not support SoftwareSerial. Only hardware uarts can be used, pins 0/1, 14/15, 16/17 or 18/19.
 
-//Note: Most Arduinos do not support higher baudrates rates than 115200.  Also the arduino hardware uarts generate 57600 and 115200 with a
-//relatively large error which can cause communications problems.
-
-//See BareMinimum example for a list of library functions
-
-//Includes required to use Roboclaw library
-#include "BMSerial.h"
-#include "RoboClaw.h"
-
-//Roboclaw Address
-#define address 0x80
 #define middleSignal 5 // signal from main that shows straight turn
-#define motorGradient 5 // difference for each level of turning, e.g. 64 to a motor plus a motorGradient of 5 = 69
+#define motorGradient 5// difference for each level of turning, e.g. 64 to a motor plus a motorGradient of 5 = 69
 #define gradientRange 4 // difference between middle and either max or min.  e.g. middle is 5, max is 9, gR = 4
-#define neutral 64 // signal for ForwardBackward to not turn either way
+#define neutral 0 // signal for ForwardBackward to not turn either way
+#define fuckedUpMotorOffset 1.1 // one motor sucks so this is the offset
 
-//Setup communcaitions with roboclaw. Use pins 10 and 11 with 10ms timeout
-RoboClaw roboclaw(10,11,10000);
+Sabertooth ST(128);
 
 void setup()
 {
-  //Open roboclaw serial ports
-  roboclaw.begin(38400);
+  SabertoothTXPinSerial.begin(9600);
+ ST.autobaud();
   
   Serial.begin(9600); // set the baud rate
+  ST.setRamping(7);
   //Serial.println("Ready"); // print "Ready" once
 }
 void loop()
@@ -42,40 +28,13 @@ void loop()
     char inByte = Serial.read(); // read the incoming data
     //Serial.println(inByte); // send the data back in a new line so that it is not all one long line
 
-	/*
-	if(inByte == '1')
-    {
-      Serial.print("drive motor");
-      //drive the motor
-      roboclaw.ForwardM1(address, 30);
-      roboclaw.ForwardM2(address, 30);
-      //Serial.println("driving motor");
-      
-    }
-    else if (inByte == '0')
-    {
-      Serial.print("stop motor");
-      roboclaw.ForwardM1(address, 0);
-      roboclaw.ForwardM2(address, 0);
-      //Serial.println("stopping motor");
-    }
-    else if(inByte == '2')
-    {
-      // right
-      Serial.print("turning right");
-      roboclaw.ForwardBackwardM1(address,30); //start Motor1 forward at half speed
-      roboclaw.ForwardBackwardM2(address, 10); //start Motor2 backward at half speed
-      
-    }
-    else if(inByte == '3')
-    {
-      Serial.println("turn left");
-      roboclaw.ForwardBackwardM1(address,10);
-      roboclaw.ForwardBackwardM2(address,30);
-    }
-	*/
-
+	
     // single digits only
+
+    if(inByte == 'a') {
+      ST.motor(1, -40);
+      ST.motor(2, -36);
+    }
 
 	if(!isDigit(inByte))
 	{
@@ -90,12 +49,12 @@ void loop()
 	if(inByte == '0')
 	{
       Serial.print("stop motor");
-      roboclaw.ForwardM1(address, 0);
-      roboclaw.ForwardM2(address, 0);
+      ST.motor(1, 0);
+      ST.motor(2, 0);
 	}
 	else
 	{
-		leftDiff = command - middleSignal;
+		leftDiff = middleSignal - command;
 		rightDiff = -1 * leftDiff;
 		if(leftDiff < 0)
 		{
@@ -106,17 +65,17 @@ void loop()
 			rightDiff = 0;
 		}
 
-		leftSignal = neutral + ((gradientRange - rightDiff) * motorGradient);
+		leftSignal = int((neutral + ((gradientRange - rightDiff) * motorGradient)) * fuckedUpMotorOffset);
 		rightSignal = neutral + ((gradientRange - leftDiff) * motorGradient);
 
-		roboclaw.ForwardBackwardM1(address,leftSignal);
-    roboclaw.ForwardBackwardM2(address,rightSignal);
+		ST.motor(1, leftSignal);
+    ST.motor(2, rightSignal);
     //Serial.printf("M1: %s, | M2: %s", leftSignal, rightSignal);  
-    Serial.print("M1: ");
-    Serial.print(leftSignal);
-    Serial.print("  | M2: ");
-    Serial.print(rightSignal);
-    Serial.print("\n"); 
+//    Serial.print("M1: ");
+//    Serial.print(leftSignal);
+//    Serial.print("  | M2: ");
+//    Serial.print(rightSignal);
+//    Serial.print("\n"); 
 	}
     
   }
